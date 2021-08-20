ff4e6d2461d096d2e9d4c8eda9fdf09d47319a61
caerus19 <77235468+caerus19@users.noreply.github.com>
2021-08-21 03:05:54 +0400

    Update loadall.py

--- userbot/modules/loadall.py
+++ userbot/modules/loadall.py
@@ -1,12 +1,13 @@
 from userbot.events import register
-from userbot import bot, TEMP_DOWNLOAD_DIRECTORY
+from userbot import bot
 import asyncio
 import os
-
+from getpass import getuser
+from time import sleep
 @register(pattern=".loadall",outgoing=True)
-async def loadall(event)
-    curruser = getuser()
-    command = "cd userbot/modules && git clone https://github.com/caerus19/UseratorPlugins"
+async def loadall(event):
+    user = getuser()
+    command = "cd userbot/modules && git clone https://github.com/caerus19/UseratorPlugins && cd UseratorPlugins && mv *.* .."
     process = await asyncio.create_subprocess_shell(
         command,
         stdout=asyncio.subprocess.PIPE,
@@ -14,4 +15,5 @@ async def loadall(event)
     stdout, stderr = await process.communicate()
     result = str(stdout.decode().strip()) \
         + str(stderr.decode().strip())
-
+    await event.edit("Hazırdır...")
+    sleep(10)
